ae7243e9d26172369160e42fd4952f8b5eeff84d
annayqho <annayqho@gmail.com>
2018-11-27 16:10:57 -0800

    no broken pow law

--- code/paper_plots/physevol_powlaw.py
+++ code/paper_plots/physevol_powlaw.py
@@ -6,8 +6,8 @@ plt.rc("font", family="serif")
 plt.rc("text", usetex=True)
 import sys
 sys.path.append("/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/code")
-from fit_arnett import load_lc
-from extrapolate_size import load_radius
+from load_lum import load_lc
+from load_radius import load_radius
 from load_temp import load_temp
 
 # Load the bolometric light curve
@@ -25,19 +25,9 @@ fig,axarr = plt.subplots(3,1, figsize=(6,8), sharex=True)
 # Plot the data
 
 # Luminosity panel
-axarr[0].errorbar(dt, lum, yerr=elum, fmt='.', c='k')
-choose = dt < 2.5
-m,b = np.polyfit(np.log10(dt[choose]), np.log10(lum[choose]), deg=1)
-xfit = np.linspace(min(dt[choose]), max(dt[choose]))
-yfit = 10**(m*np.log10(xfit)+b)
-axarr[0].plot(xfit, yfit, ls='--', c='grey')
-axarr[0].text(1E-1, 1E44, '$t^{-0.17}$',
-        horizontalalignment='left', verticalalignment='center', fontsize=14)
-
-choose = dt > 2.5
-m,b = np.polyfit(np.log10(dt[choose]), np.log10(lum[choose]), deg=1)
+m,b = np.polyfit(np.log10(dt), np.log10(lum), deg=1)
 m = -5/3
-xfit = np.linspace(min(dt[choose]), max(dt[choose]))
+xfit = np.linspace(min(dt), max(dt))
 yfit = 10**(m*np.log10(xfit)+b)
 axarr[0].plot(xfit, yfit, ls='--', c='grey')
 axarr[0].text(10, 2E43, '$t^{-5/3}$',
@@ -61,19 +51,9 @@ axarr[1].plot(xvals, yvals, ls='--', c='grey')
 axarr[1].text(1, 2E14, 'v=0.1c', fontsize=12)
 
 # Temperature panel
-axarr[2].errorbar(dt, temp, yerr=etemp, fmt='.', c='k')
-choose = dt < 2.5
-m,b = np.polyfit(np.log10(dt[choose]), np.log10(temp[choose]), deg=1)
-xfit = np.linspace(min(dt[choose]), max(dt[choose]))
-yfit = 10**(m*np.log10(xfit)+b)
-axarr[2].plot(xfit, yfit, ls='--', c='grey')
-axarr[2].text(1E-1, 2E4, '$t^{-0.11}$',
-        horizontalalignment='left', verticalalignment='center', fontsize=14)
-
-choose = np.logical_and(dt > 2.5, dt < 11)
-m,b = np.polyfit(np.log10(dt[choose]), np.log10(temp[choose]), deg=1)
+m,b = np.polyfit(np.log10(dt), np.log10(temp), deg=1)
 print(m)
-xfit = np.linspace(min(dt[choose]), max(dt[choose]))
+xfit = np.linspace(min(dt), max(dt))
 yfit = 10**(m*np.log10(xfit)+b)
 axarr[2].plot(xfit, yfit, ls='--', c='grey')
 axarr[2].text(10, 1E4, '$t^{-0.89}$',
@@ -91,15 +71,14 @@ for ii in np.arange(0,3):
     axarr[ii].set_yscale('log')
 
 axarr[1].set_ylim(1E14, 1E16)
-axarr[2].set_xlim(2E-3, 50)
+axarr[2].set_xlim(2E-1, 50)
 axarr[2].set_xscale('log')
 axarr[2].set_xlabel(r'Days since $t_0$', fontsize=16)
 axarr[0].set_ylabel(r'$L_\mathrm{bol}$ (erg/s)', fontsize=16)
 axarr[1].set_ylabel(r'$R_\mathrm{ph}$ (cm)', fontsize=16)
 axarr[2].set_ylabel(r'$T_\mathrm{eff}$ (K)', fontsize=16)
 
-
 plt.subplots_adjust(hspace=0)
 plt.tight_layout()
-#plt.show()
-plt.savefig("bbfit_log.png")
+plt.show()
+#plt.savefig("bbfit_log.png")
