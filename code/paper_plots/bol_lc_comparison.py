17fd58c3d65e2859744dd2d92b1a6781681a0c79
annayqho <annayqho@gmail.com>
2018-11-29 10:17:36 -0800

    added 2008D

--- code/paper_plots/bol_lc_comparison.py
+++ code/paper_plots/bol_lc_comparison.py
@@ -10,14 +10,38 @@ from astropy.cosmology import Planck15
 sys.path.append("/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/code")
 from load_lum import load_lc
 
+# Where the bolometric light curve compilation lives
+ddir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/bol_lc"
+
+# Initialize figure
+fig,ax = plt.subplots(1,1, figsize=(8,6), sharex=True)
 
 # Plot the bolometric light curve of ZTF18abukavn
 dt, lum, llum, ulum = load_lc()
-
-fig,ax = plt.subplots(1,1, figsize=(8,6), sharex=True)
 ax.errorbar(dt, lum, yerr=[llum,ulum], fmt='.', c='k')
 ax.text(dt[0], lum[0], 'AT2018gep', fontsize=14)
 
+# Next: SN2008D
+dat = Table.read(
+        ddir + "/sn2008d.dat", delimiter='&', format='ascii.fast_no_header')
+dt = dat['col1']
+TBB_raw = dat['col2']
+TBB = np.array([val.split('+')[0] for val in TBB_raw])
+uTBB = np.array([val.split('+')[1].split('_')[0] for val in TBB_raw])
+lTBB = np.array([val.split('+')[1].split('_')[1] for val in TBB_raw])
+lum_raw = dat['col4']
+lum = 10**(np.array(
+    [val.split('+')[0] for val in lum_raw]).astype(float))
+ulum = 10**(np.array(
+    [val.split('+')[1].split('_')[0] for val in lum_raw]).astype(float))
+llum = 10**(np.array(
+    [val.split('+')[1].split('_')[1] for val in lum_raw]).astype(float))
+ax.errorbar(dt, lum, yerr=[llum,ulum], fmt='.', c='grey')
+ax.plot(dt, lum, c='grey')
+#ax.fill_between(dt, y1=lum-ulum, y2=lum+llum, color='grey')
+ax.text(dt[0], lum[0], 'SN2008D', fontsize=14)
+
+# Formatting
 ax.tick_params(axis='both', labelsize=12)
 ax.set_yscale('log')
 ax.set_xscale('log')
