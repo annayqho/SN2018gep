678f03a06a72bcc535dba16e86a840e3ed49546d
annayqho <annayqho@gmail.com>
2019-02-21 09:33:19 -0800

    plot all as grey background

--- code/paper_plots/plot_lc.py
+++ code/paper_plots/plot_lc.py
@@ -12,6 +12,13 @@ from astropy.cosmology import Planck15
 import glob
 
 zp = 2458370.6473
+# extinction values
+ext = {}
+ext['u'] = 0.045
+ext['g'] = 0.035
+ext['r'] = 0.024
+ext['i'] = 0.018
+ext['z'] = 0.013
 
 
 def plot_inset():
@@ -75,70 +82,46 @@ def plot_lc():
     det = np.logical_and(mag<99, ~np.isnan(mag))
     nondet = np.logical_or(mag==99, np.isnan(mag))
 
-    fig,ax = plt.subplots(1,1,figsize=(8,5))
-    rcol = 'k'
-    ucol = '#f98e09'
+    fig,axarr = plt.subplots(
+            2, 2, figsize=(8,8), sharex=True, sharey=True)
 
-    band = filt=='u'
-    ext = 0.045
-    choose = np.logical_and(det, band)
-    ax.errorbar(
-            dt[choose], mag[choose]-ext, emag[choose], fmt='o', ms=5,
-            mec=ucol, mfc=ucol, c=ucol, label='u', zorder=8)
+    # for each panel, plot all of them as a grey background
+    for ax in axarr:
+        for f in band:
+            choose = np.logical_and(det, filt == f)
+            ax.plot(
+                    dt[choose], mag[choose]-ext[f], c='lightgrey',
+                    alpha=0.7)
 
-    band = filt=='g'
-    ext = 0.035
-    choose = np.logical_and(det, band)
-    ax.errorbar(
-            dt[choose], mag[choose]-ext, emag[choose], fmt='o', ms=5,
-            mec='#57106e', mfc='white', c='#57106e', label='g')
+#     choose = np.logical_and(det, band)
+#     ax.errorbar(
+#             dt[choose], mag[choose]-ext[f], emag[choose], fmt='s', ms=6,
+#             mec=rcol, mfc=rcol, c=rcol, label='r', zorder=9)
 
-    band = filt=='r'
-    ext = 0.024
-    choose = np.logical_and(det, band)
-    ax.errorbar(
-            dt[choose], mag[choose]-ext, emag[choose], fmt='s', ms=6,
-            mec=rcol, mfc=rcol, c=rcol, label='r', zorder=9)
-    choose = np.logical_and(nondet, band)
-    # value of limiting mag: 20.47
-    ax.arrow(
-            2458370.6408-zp, 19.97, 0, 0.5, length_includes_head=True,
-            head_width=0.01, head_length=0.1, fc='k', ec='k')
+#     ax.arrow(
+#             2458370.6408-zp, 19.97, 0, 0.5, length_includes_head=True,
+#             head_width=0.01, head_length=0.1, fc='k', ec='k')
 
-    band = filt=='i'
-    ext = 0.018
-    choose = np.logical_and(det, band)
-    ax.errorbar(
-            dt[choose], mag[choose]-ext, emag[choose], fmt='v', ms=5,
-            c='grey', label='i')
-     
-    band = filt=='z'
-    ext = 0.013
-    choose = np.logical_and(det, band)
-    ax.errorbar(
-            dt[choose], mag[choose]-ext, emag[choose], fmt='s', ms=6,
-            mec='k', mfc='white', label='z', c='k', zorder=1)
+   #  ax2 = ax.twinx()
+   #  ax2.set_ylabel(
+   #          "Absolute Magnitude",
+   #          fontsize=14, rotation=270, labelpad=15.0)
+   #  y_f = lambda y_i: y_i-Planck15.distmod(z=0.033).value
+   #  ymin, ymax = ax.get_ylim()
+   #  ax2.set_ylim((y_f(ymin), y_f(ymax)))
+   #  ax2.plot([],[])
+   #  ax2.tick_params(axis='both', labelsize=14)
 
-    ax2 = ax.twinx()
-    ax2.set_ylabel(
-            "Absolute Magnitude",
-            fontsize=14, rotation=270, labelpad=15.0)
-    y_f = lambda y_i: y_i-Planck15.distmod(z=0.033).value
-    ymin, ymax = ax.get_ylim()
-    ax2.set_ylim((y_f(ymin), y_f(ymax)))
-    ax2.plot([],[])
-    ax2.tick_params(axis='both', labelsize=14)
-
-    ax.set_ylabel("Apparent Magnitude", fontsize=16)
-    ax.set_xlabel(
-        r"Days since $t_0=$JD 2458370.6473 (UT 2018 Sept 09.15)", fontsize=16)
-    ax.yaxis.set_tick_params(labelsize=14)
-    ax.xaxis.set_tick_params(labelsize=14)
-    ax.legend(loc='upper right', fontsize=12)
+   #  ax.set_ylabel("Apparent Magnitude", fontsize=16)
+   #  ax.set_xlabel(
+   #      r"Days since $t_0=$JD 2458370.6473 (UT 2018 Sept 09.15)", fontsize=16)
+   #  ax.yaxis.set_tick_params(labelsize=14)
+   #  ax.xaxis.set_tick_params(labelsize=14)
+   #  ax.legend(loc='upper right', fontsize=12)
     #ax.set_xscale('log')
     #ax.set_xlim(0, 80)
-    ax.invert_yaxis()
-    ax2.invert_yaxis()
+   #  ax.invert_yaxis()
+   #  ax2.invert_yaxis()
 
     plt.tight_layout()
     #plt.savefig("lc.png")
