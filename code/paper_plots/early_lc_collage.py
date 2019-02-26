8fbe4ec2458edb80577db339b1ab56fcaaf72d28
annayqho <annayqho@gmail.com>
2019-02-26 13:23:49 -0800

    move to a function

--- code/paper_plots/early_lc_collage.py
+++ code/paper_plots/early_lc_collage.py
@@ -11,30 +11,39 @@ fig, axarr = plt.subplots(2,2,sharex=True,sharey=True)
 
 datadir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/lc"
 
-# Plot the GRB 980425 early LC
-dat = ascii.read(
-    datadir + "/lc_980425.txt", format="fixed_width", delimiter="&")
-jd_raw = np.array(dat['JD'])
-delta = jd_raw[0] - 17/24 # first obs is 17 hrs after burst
-jd = jd_raw - delta
-bands = ['B','Vc','Rc']
-shapes = ['o', 'o', 's']
-colors = ['black', 'white', 'black']
-ls = ['-', '--', ':']
-#colors = ['blue', 'green', 'red', 'lightsalmon']
-for ii,band in enumerate(bands):
+
+def get_98bw_lc(band):
+    dat = ascii.read(
+        datadir + "/lc_980425.txt", format="fixed_width", delimiter="&")
+    jd_raw = np.array(dat['JD'])
+    delta = jd_raw[0] - 17/24 # first obs is 17 hrs after burst
+    jd = jd_raw - delta
     lc = dat[band]
     bad = np.array(['nodata' in val for val in lc])
     mag = np.array([float(val.split("$\\pm$")[0]) for val in lc[~bad]])
     mag_err = np.array(
             [float(val.split("$\\pm$")[1]) for val in lc[~bad]])
     t = jd[~bad]
-    for ax in axarr.reshape(-1):
-        ax.plot(t,mag,c='k', linestyle=ls[ii])
-        ax.errorbar(
-                t,mag,yerr=mag_err,ecolor='k', fmt=shapes[ii],
-                mfc=colors[ii],mec='k',label=band,ms=5)
-        ax.legend()
+    return t, mag, mag_err
+
+
+# Plot the GRB 980425 r-band early LC in each panel,
+# color-coded by the Vc-Rc color
+t, mag, mag_err = get_98bw_lc('Rc')
+vt, vmag, vmag_err = get_98bw_lc('Vc')
+for ax in axarr.reshape(-1):
+    ax.plot(t, mag, c='lightgrey', alpha=0.7)
+ax = axarr.reshape(-1)[0]
+ax.errorbar(
+    t, mag, yerr=mag_err, c='k', fmt='o', ms=3, alpha=1.0)
+ax.text(
+        0.9, 0.9, "1998bw, r-band", 
+        fontsize=14, transform=ax.transAxes,
+        horizontalalignment='right',
+        verticalalignment='top',
+        bbox=dict(
+            boxstyle="round", fc='white', ec='k', 
+            lw=0.5, alpha=1.0, pad=0.4))
 
 ax.set_xlim(0,4)
 ax.set_ylim(15,16.5)
