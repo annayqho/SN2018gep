44261c9fa3388d20854f5a95e1afac44455db0ce
annayqho <annayqho@gmail.com>
2019-03-05 15:51:20 -0800

    misc

--- code/paper_plots/prog_lc.py
+++ code/paper_plots/prog_lc.py
@@ -1,6 +1,6 @@
 """ 
-Fit for the explosion epoch by fitting a polynomial 
-to the g-band light curve """
+Plot the full early LC, including progenitor stuff
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,66 +10,84 @@ from matplotlib import rc
 rc("font", family="serif")
 rc("text", usetex=True)
 from astropy.cosmology import Planck15
+from astropy.io import ascii
 
 d = Planck15.luminosity_distance(z=0.033).cgs.value
+# JD of first (r-band) detection
+t0 = 2458370.6634
+
+def mag_to_flux(mag, emag):
+    """ Convert from mag to flux """
+    # AB flux zero point for g-band is 3631 Jy or 1E-23 erg/cm2/s/Hz
+    flux = 10**(-(mag + 48.6)/2.5)
+    lum = 4 * np.pi * d**2 * flux
+    # Uncertainty in magnitude is roughly the fractional uncertainty on the flux
+    eflux = emag*flux
+    elum = 4 * np.pi * d**2 * eflux
+    return lum, elum
 
 DATA_DIR = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/phot"
 f = DATA_DIR + "/ZTF18abukavn_opt_phot.dat"
 dat = np.loadtxt(f, dtype=str, delimiter=' ')
 instr = dat[:,0]
 jd = dat[:,1].astype(float)
+dt = jd-t0
 filt = dat[:,2]
 mag = dat[:,3].astype(float)
 emag = dat[:,4].astype(float)
-
 det = np.logical_and(mag<99, ~np.isnan(mag))
-# JD of first (r-band) detection
-t0 = 2458370.6634
-dt = jd-t0
+
+# Progenitor data
+f = DATA_DIR + "/precursor.csv"
+prog = ascii.read(f)
+prog_mjd = prog['mjd']
+prog_jd = prog_mjd + 2400000.5
+prog_dt = prog_jd - t0
+prog_filter = prog['filter']
+prog_mag = prog['mag']
+prog_emag = prog['magerr']
+prog_limmag = prog['lim_mag']
+code = prog['instrument']
+prog_det = np.logical_and(prog_dt < 0, ~np.isnan(prog_mag))
+prog_nondet = np.logical_and(prog_dt < 0, np.isnan(prog_mag))
 
 # Initialize the figure
-fig,axarr = plt.subplots(2,1,figsize=(5,6))
+fig,axarr = plt.subplots(3,1,figsize=(8,8))
 
-# Convert from mag to flux
-# AB flux zero point for g-band is 3631 Jy or 1E-23 erg/cm2/s/Hz
-flux = 10**(-(mag + 48.6)/2.5)
-lum = 4 * np.pi * d**2 * flux
-# Uncertainty in magnitude is roughly the fractional uncertainty on the flux
-eflux = emag*flux
-elum = 4 * np.pi * d**2 * eflux
 
 ax = axarr[0]
 # Plot the zoom-in
 gband = np.logical_and(instr=='P48+ZTF', filt=='g')
 choose = np.logical_and(det, gband)
 ax.errorbar(
-        dt[choose]*24*60, lum[choose]/1E28, yerr=elum[choose]/1E28, 
+        dt[choose]*24*60, mag[choose], yerr=emag[choose], 
         c='k', ms=5, fmt='s', label="P48 $g$", zorder=2)
 
 # Plot the r-band detections
 rband = np.logical_and(instr=='P48+ZTF', filt=='r')
 choose = np.logical_and(det, rband)
 ax.errorbar(
-        dt[choose]*24*60, lum[choose]/1E28, yerr=elum[choose]/1E28, 
+        dt[choose]*24*60, mag[choose], yerr=emag[choose], 
         ms=5, fmt='o', mfc='white', mec='grey', label="P48 $r$", c='grey',
         zorder=0)
 
 # Show the last non-detection
 ax.axvline(x=-32.5, ls=':', c='grey')
-ax.text(-32.5, 0.3, 'ND', fontsize=14,
+ax.text(-32.5, 19, 'ND', fontsize=14,
         horizontalalignment='center',
         verticalalignment='center')
 ax.axvline(x=-23, ls='--', c='k')
-ax.text(-18, 0.3, '$t_0$', fontsize=16, horizontalalignment='center',
+ax.text(-18, 19, '$t_0$', fontsize=16, horizontalalignment='center',
         verticalalignment='center')
 
 # Format this box
 ax.set_xlim(-40, 80)
-ax.set_ylim(0, 0.35)
-ax.set_ylabel(r"$L_\nu$ [$10^{28}$ erg/s/Hz]", fontsize=16)
+ax.set_ylim(18.5,21)
+ax.set_ylabel(r"Apparent Mag", fontsize=16)
 ax.set_xlabel("Minutes since first detection", fontsize=16)
 ax.yaxis.set_tick_params(labelsize=14)
 ax.xaxis.set_tick_params(labelsize=14)
+ax.invert_yaxis()
 ax.legend(loc='lower right', fontsize=14)
 
 ax = axarr[1]
