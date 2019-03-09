5aa154d28e7a8267e462d56161b631cdbfcb5b36
annayqho <annayqho@gmail.com>
2019-03-08 17:03:11 -0800

    plot day 2 spectrum

--- code/paper_plots/spec_day2.py
+++ code/paper_plots/spec_day2.py
@@ -13,24 +13,15 @@ from spec_sequence import *
 
 fig = plt.figure(figsize=(8,4))
 
-cow_dir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/spec/AT2018cow"
-wl_cow, flux_cow, ivar_cow = load_spec(
-        cow_dir + "/AT2018cow_20180621_P200_v3.ascii", 'P200')
-
-gepfile = "ZTF18abukavn_20180910_P200_v6.ascii"
+gepfile = "ZTF18abukavn_20180911_P200_v2.ascii"
 #gepfile = "ZTF18abukavn_20180909_LT_v1.ascii"
 gep_dir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/spec/ZTF18abukavn"
 wl_gep, flux_gep, ivar_gep = load_spec(gep_dir + "/%s" %gepfile, 'P200')
 wl_gep, flux_gep = clip_lines(wl_gep, flux_gep, 0.03154, 'P200', 1.0)
 
 plt.plot(
-        wl_cow, flux_cow/4.7/1E-15,
-        c='grey', drawstyle='steps-mid', lw=0.5, 
-        label="AT2018cow/4.7, $\Delta t=5.353$")
-plt.plot(
         wl_gep, flux_gep/1E-15,
-        c='k', drawstyle='steps-mid', lw=0.5,
-        label="AT2018gep, $\Delta t=1.0$")
+        c='k', drawstyle='steps-mid', lw=0.5)
 
 plt.tick_params(labelsize=14)
 plt.xlim(3700, 7000)
@@ -42,6 +33,6 @@ plt.ylabel(
 plt.legend(fontsize=14)
 
 plt.tight_layout()
-plt.savefig("spec_comp_18cow.png")
-#plt.show()
+#plt.savefig("spec_day2.png")
+plt.show()
 
