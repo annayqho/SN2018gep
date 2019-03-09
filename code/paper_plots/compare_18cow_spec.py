a670c15a75011248afe91039c636b9f2f2a7cb64
annayqho <annayqho@gmail.com>
2019-03-08 18:16:33 -0800

    plot smoothed version

--- code/paper_plots/compare_18cow_spec.py
+++ code/paper_plots/compare_18cow_spec.py
@@ -11,22 +11,28 @@ import glob
 from spec_sequence import *
 
 
-fig = plt.figure(figsize=(8,4))
+fig,ax = plt.subplots(1,1,figsize=(8,4))
 
 cow_dir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/spec/AT2018cow"
 wl_cow, flux_cow, ivar_cow = load_spec(
         cow_dir + "/AT2018cow_20180621_P200_v3.ascii", 'P200')
-
-gepfile = "ZTF18abukavn_20180910_P200_v6.ascii"
-#gepfile = "ZTF18abukavn_20180909_LT_v1.ascii"
-gep_dir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/spec/ZTF18abukavn"
-wl_gep, flux_gep, ivar_gep = load_spec(gep_dir + "/%s" %gepfile, 'P200')
-wl_gep, flux_gep = clip_lines(wl_gep, flux_gep, 0.03154, 'P200', 1.0)
-
 plt.plot(
         wl_cow, flux_cow/4.7/1E-15,
         c='grey', drawstyle='steps-mid', lw=0.5, 
         label="AT2018cow/4.7, $\Delta t=5.353$")
+
+gepfile = "ZTF18abukavn_20180910_P200_v6.ascii"
+gep_dir = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/spec/ZTF18abukavn"
+tel = 'P200'
+dt = 1.0
+wl_gep, flux_gep, ivar_gep = load_spec(gep_dir + "/%s" %gepfile, tel)
+wl, flux = fluxcal(wl, flux, dt)
+wl, flux = clip_lines(wl, flux, 0.03154, 'P200', dt)
+wl, flux = clip_tellurics(wl, flux)
+plot_spec(ax, wl, flux/1E-15, 'P200', dt)
+plot_smoothed_spec(
+        ax, wl, flux/1E-15, ivar, 'P200', dt)
+wl_gep, flux_gep = clip_lines(wl_gep, flux_gep, 0.03154, tel, dt)
 plt.plot(
         wl_gep, flux_gep/1E-15,
         c='k', drawstyle='steps-mid', lw=0.5,
@@ -42,6 +48,6 @@ plt.ylabel(
 plt.legend(fontsize=14)
 
 plt.tight_layout()
-plt.savefig("spec_comp_18cow.png")
-#plt.show()
+#plt.savefig("spec_comp_18cow.png")
+plt.show()
 
