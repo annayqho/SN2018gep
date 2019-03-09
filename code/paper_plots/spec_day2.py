6b00917d4122c75b443a1c3eedd13a4a5b2c7c0f
annayqho <annayqho@gmail.com>
2019-03-08 17:21:25 -0800

    label the spec

--- code/paper_plots/spec_day2.py
+++ code/paper_plots/spec_day2.py
@@ -24,12 +24,19 @@ plot_spec(ax, wl, flux/1E-15, 'P200', 2.07)
 plot_smoothed_spec(
         ax, wl, flux/1E-15, ivar, 'P200', 2.07)
 
-#plt.plot(
-#        wl, flux/1E-15, c='k', drawstyle='steps-mid', lw=0.5)
+ax.text(0.9, 0.9, r"SN2018gep at $\Delta t=2.07$d (P200)", fontsize=14,
+        transform=ax.transAxes, horizontalalignment='right',
+        verticalalignment='top')
+ax.text(0.9, 0.8, r"$T_\mathrm{eff}\approx30,000$\,K", fontsize=14,
+        transform=ax.transAxes, horizontalalignment='right',
+        verticalalignment='top')
+ax.text(0.9, 0.7, r"$v\approx35,000\,$km/s", fontsize=14,
+        transform=ax.transAxes, horizontalalignment='right',
+        verticalalignment='top')
 
 plt.tick_params(labelsize=14)
-plt.xlim(3700, 7000)
-plt.ylim(0.3, 4.1)
+plt.xlim(3200, 9320)
+plt.ylim(0.015, 8.7)
 plt.xlabel(r"Observed Wavelength (\AA)", fontsize=16)
 plt.ylabel(
     r"Flux $f_{\lambda}$ ($10^{-15}$ erg cm$^{-2}$ s$^{-1}$ \AA$^{-1}$)", 
