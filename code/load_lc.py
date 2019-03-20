685b2ae9215b18e84129dbfc34c69fbbb851bb34
annayqho <annayqho@gmail.com>
2019-03-20 08:51:44 -0700

    initial commit

---
+++ code/load_lc.py
@@ -0,0 +1,62 @@
+""" Load the individual light curve at a given band """
+
+import numpy as np
+from astropy.cosmology import Planck15
+
+zp = 2458370.6473
+d = Planck15.luminosity_distance(z=0.03154).cgs.value
+
+def get_uv_lc():
+    DATA_DIR = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/uv"
+
+    # Dictionary mapping the filter to the wavelength
+    bands = {}
+    bands['V'] = 5468
+    bands['B'] = 4392
+    bands['U'] = 3465
+    bands['UVW1'] = 2600
+    bands['UVM2'] = 2246
+    bands['UVW2'] = 1928
+
+    f = DATA_DIR + "/UVOT_lightcurve_maghist_full_hostsub.ascii"
+    dat = np.loadtxt(f, dtype=str, delimiter=' ')
+    jd = dat[:,0].astype(float)
+    filt = dat[:,1]
+    wl = np.array([bands[val] for val in filt]) # ang
+    nu = 3E18 / wl # Hz
+    fnu_mjy = dat[:,2].astype(float)
+    efnu_mjy = dat[:,3].astype(float)
+    fact = 1E-3 * 1E-23 * 4 * np.pi * d**2 
+    lnu = fnu_mjy * fact 
+    elnu = efnu_mjy * fact 
+
+    zp = 2458370.6473
+    dt = jd-zp
+
+    return dt, filt, fnu_mjy, efnu_mjy
+
+
+def get_lc():
+    # get optical light curves 
+    DATA_DIR = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/phot"
+    f = DATA_DIR + "/ZTF18abukavn_opt_phot.dat"
+    dat = np.loadtxt(f, dtype=str, delimiter=' ')
+    instr = dat[:,0]
+    jd = dat[:,1].astype(float)
+    filt = dat[:,2]
+    mag = dat[:,3].astype(float)
+    emag = dat[:,4].astype(float)
+    dt = jd-zp
+    
+    # add the UV light curves
+    add_dt, add_filt, fnu_mjy, efnu_mjy = get_uv_lc()
+    # convert to AB mag
+    add_mag = -2.5 * np.log10(fnu_mjy*1E-3) + 8.90
+    add_emag = (efnu_mjy/fnu_mjy) # I think it's just the ratio
+    choose = add_emag < 50
+    dt = np.append(dt, add_dt[choose])
+    filt = np.append(filt, add_filt[choose])
+    mag = np.append(mag, add_mag[choose])
+    emag = np.append(emag, add_emag[choose])
+
+    return dt, filt, mag, emag
