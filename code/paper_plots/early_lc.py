b70195cedd8ddc3322a6f782394c1dfb77a87b8e
annayqho <annayqho@gmail.com>
2019-03-26 10:25:56 -0700

    show even the sub-3sig individual image detections

--- code/paper_plots/early_lc.py
+++ code/paper_plots/early_lc.py
@@ -31,83 +31,75 @@ def mag_to_flux(mag, emag):
 def full_lc():
     """ Plot the full LC in g and r, showing all the progenitor detections """
     DATA_DIR = "/Users/annaho/Dropbox/Projects/Research/ZTF18abukavn/data/phot"
-    f = DATA_DIR + "/ZTF18abukavn_opt_phot.dat"
-    dat = np.loadtxt(f, dtype=str, delimiter=' ')
-    instr = dat[:,0]
-    jd = dat[:,1].astype(float)
-    dt = jd-t0
-    filt = dat[:,2]
-    mag = dat[:,3].astype(float)
-    emag = dat[:,4].astype(float)
-    det = np.logical_and(mag<99, ~np.isnan(mag))
 
-    # Progenitor data
+    # Full light curve from Danny
     f = DATA_DIR + "/ZTF18abukavn.csv"
-    prog = ascii.read(f)
-    prog_mjd = prog['mjd']
-    prog_jd = prog_mjd + 2400000.5
-    prog_dt = prog_jd - t0
-    prog_filter = prog['filter']
-    prog_mag = prog['mag']
-    prog_emag = prog['magerr']
-    prog_limmag = prog['lim_mag']
-    code = prog['instrument']
-    prog_det = np.logical_and(prog_dt < 0, ~np.isnan(prog_mag))
-    prog_nondet = np.logical_and(code=='final photometry', np.isnan(prog_mag))
+    dat = ascii.read(f)
+    mjd = dat['mjd']
+    jd = mjd + 2400000.5
+    dt = jd - t0
+    filt = dat['filter']
+    mag = dat['mag']
+    emag = dat['magerr']
+    limmag = dat['lim_mag']
+    code = dat['instrument']
+    sn_det = np.logical_and.reduce(
+            (code=='ZTF Camera', dt > -1, ~np.isnan(mag)))
+    prog_det = np.logical_and.reduce(
+            (code=='final photometry', dt < -1, ~np.isnan(mag)))
+    prog_nondet = np.logical_and(code=='final photometry', np.isnan(mag))
 
     # Initialize the figure
-    fig,ax = plt.subplots(1,1,figsize=(5,3))
+    fig,ax = plt.subplots(1,1,figsize=(10,3))
 
     # Plot the g-band LC
-    gband = np.logical_and(instr=='P48+ZTF', filt=='g')
-    choose = np.logical_and(det, gband)
+    choose = np.logical_and(sn_det, filt=='ztfg')
     ax.errorbar(
             dt[choose], mag[choose], yerr=emag[choose], 
             c='k', ms=5, fmt='s', label="P48 $g$", zorder=2)
 
     # Plot the r-band detections
-    rband = np.logical_and(instr=='P48+ZTF', filt=='r')
-    choose = np.logical_and(det, rband)
+    choose = np.logical_and(sn_det, filt=='ztfr')
     ax.errorbar(
             dt[choose], mag[choose], yerr=emag[choose], 
             ms=5, fmt='o', mfc='white', mec='grey', label="P48 $r$", c='grey',
             zorder=0)
 
     # Plot the r-band non-detection, 32.5 min before the first det
-    rband = np.logical_and(instr=='P48+ZTF', filt=='r')
-    choose = np.logical_and(~det, rband)
-    xnondet = -32.5/60/24
-    ynondet = 21.25
-    ax.scatter(
-            xnondet, ynondet, color='grey', marker='_', label=None, s=20)
-    ax.arrow(
-            xnondet, ynondet, 0, 0.7, length_includes_head=True,
-            head_width=1, head_length=0.2, fc='grey', ec='grey')
+    # rband = np.logical_and(instr=='P48+ZTF', filt=='r')
+    # choose = np.logical_and(~det, rband)
+    # xnondet = -32.5/60/24
+    # ynondet = 21.25
+    # ax.scatter(
+    #         xnondet, ynondet, color='grey', marker='_', label=None, s=20)
+    # ax.arrow(
+    #         xnondet, ynondet, 0, 0.7, length_includes_head=True,
+    #         head_width=1, head_length=0.2, fc='grey', ec='grey')
 
     # Plot the g-band prog LC
-    choose = np.logical_and(prog_det, prog_filter=='ztfg')
+    choose = np.logical_and(prog_det, filt=='ztfg')
     ax.errorbar(
-            prog_dt[choose], prog_mag[choose], yerr=prog_emag[choose], 
+            dt[choose], mag[choose], yerr=emag[choose], 
             c='k', ms=5, fmt='s', label=None, zorder=2)
 
     # Plot the g-band prog non-detections
-    choose = np.logical_and(prog_nondet, prog_filter=='ztfg')
+    choose = np.logical_and(prog_nondet, filt=='ztfg')
     ax.scatter(
-            prog_dt[choose], prog_limmag[choose], 
+            dt[choose], limmag[choose], 
             color='k', 
             s=20, marker='_', label=None, zorder=2)
 
     # Plot the r-band prog LC
-    choose = np.logical_and(prog_det, prog_filter=='ztfr')
+    choose = np.logical_and(prog_det, filt=='ztfr')
     ax.errorbar(
-            prog_dt[choose], prog_mag[choose], yerr=prog_emag[choose], 
+            dt[choose], mag[choose], yerr=emag[choose], 
             ms=5, fmt='o', mfc='white', mec='grey', label=None, c='grey',
             zorder=0)
 
     # Plot the r-band prog non-detections
-    choose = np.logical_and(prog_nondet, prog_filter=='ztfr')
+    choose = np.logical_and(prog_nondet, filt=='ztfr')
     ax.scatter(
-            prog_dt[choose], prog_limmag[choose], 
+            dt[choose], limmag[choose], 
             color='grey', 
             s=10, marker='_', label=None, zorder=2)
 
@@ -124,7 +116,7 @@ def full_lc():
     ax2.invert_yaxis()
 
     # Format this box
-    ax.set_xlim(-10, 33)
+    ax.set_xlim(-175, 33)
     ax.set_ylabel(r"Apparent Mag", fontsize=16)
     ax.set_xlabel("Days since $t_0$", fontsize=16)
     ax.yaxis.set_tick_params(labelsize=14)
